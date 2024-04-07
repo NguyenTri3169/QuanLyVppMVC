243715e26587ed422dbb3647003d8563dc716554
NguyenTri3169 <nguyentri3169@gmail.com>
2024-04-07 11:36:54 +0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using QuanLyVppMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QuanLyVppMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        QLTVEntities1 db = new QLTVEntities1(); 
+        public ActionResult Index()
+        {
+            List<SACH> ds = db.SACHes.ToList();
+            return View(ds);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
